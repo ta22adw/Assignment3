0e21f49f17e79405fed6bcebf42140abff638bea
ta22adw <126515472+ta22adw@users.noreply.github.com>
2023-05-10 11:02:13 +0100

    Add files via upload
    Clustering and fitting

---
+++ Assignment 3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 7 10:56:52 2023
+
+@author: timothyatoyebi 22027370
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from scipy.optimize import curve_fit
